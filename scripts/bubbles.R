6f1f787ed99e93afe4d6be9ee779b42fbe543342
Hiz Jamali <hiz.jamali@csiro.au>
2018-08-08 11:30:25 +1000

    added box plot scripts

--- scripts/bubbles.R
+++ scripts/bubbles.R
@@ -8,19 +8,43 @@ bubbles <- read_csv(("data/bubbles.csv"))            #upload data
 
 bubbles
 
-#
-nanobubbles <- as_tibble(bubbles)
-bubbles_plot1 <- ggplot(data = nanobubbles,
-                       aes(x = date(date), y = height_cm, colour = treatment)) +
-  geom_point(size = 2) +
+
+#Effect of bubbles on plant height
+nanobubbles <- as_tibble(bubbles)  
+  
+nanobubbles %>% 
+  #filter(datetime %in% c("23/07/2013")) %>%
+  ggplot(aes(x = datetime, y = height_cm, colour = treatment)) +
+  geom_boxplot(size = 1) +
   scale_y_continuous(limits =c(0,110)) +
   facet_wrap(~species) +
   labs(                                      #labels 
     x= "Date",
     y = "Height (cm)",
-    title = "Corn",
+    title = "Effect of Bubbles on Plant Height",
     colour = "Treatment") +
   theme(panel.grid = element_blank(), #remove panel lines
-        axis.text.x = element_text(angle = 75, size=7),
+        axis.text.x = element_text(angle = 45, size=10),
         panel.background = element_rect(fill = "white",
                                         colour = "black"))
+  
+#Effect of bubbles on number of leaves
+nanobubbles <- as_tibble(bubbles)  
+
+nanobubbles %>% 
+  #filter(datetime %in% c("23/07/2013")) %>%
+  ggplot(aes(x = datetime, y = leaves, colour = treatment)) +
+  geom_boxplot(size = 1) +
+  scale_y_continuous(limits =c(0,10)) +
+  facet_wrap(~species) +
+  labs(                                      #labels 
+    x= "Date",
+    y = "Number of leaves",
+    title = "Effect of Bubbles on Number of Leaves",
+    colour = "Treatment") +
+  theme(panel.grid = element_blank(), #remove panel lines
+        axis.text.x = element_text(angle = 45, size=10),
+        panel.background = element_rect(fill = "white",
+                                        colour = "black"))
+
+
